506676df7a29667f4427779be9ee674e18e30721
Payam Yasaie <payam.yasaie@outlook.com>
2019-06-16 16:38:02 +0430

    bug fixed

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Constraint;
+use Intervention\Image\Image;
+use Intervention\Image\ImageCache;
 
 /**
  * @author Payam Yasaie <payam@yasaie.ir>
@@ -21,36 +24,38 @@ class PublicController extends Controller
     {
         $scale = 50;
         $cache_time = (60 * 24 * 10); # 10 days
+        $file_dir = 'C:\Users\yasaie\Desktop\Stock Photos\\';
 
         $validator = \Validator::make($request->all(), [
-            'h' => 'required|int|min:' . $scale,
-            'w' => 'int|min:' . $scale,
+            'h' => 'required_if:c,1|int|min:' . $scale,
+            'w' => 'required_if:c,1|int|min:' . $scale,
             'c' => 'bool'
         ]);
 
         if (!$validator->fails()) {
-            $height = (round($request->h / $scale)) * $scale;
+            $height = $request->h ? ((round($request->h / $scale)) * $scale) : 0;
             $width = $request->w ? ((round($request->w / $scale)) * $scale) : 0;
             $crop = $request->c ?: 0;
 
-            $img = \Image::cache(function($img) use ($image, $ext, $height, $width, $crop) {
-                $img->make('C:\Users\yasaie\Desktop\Stock Photos\\' . $image .'.'. $ext);
+            $img = \Image::cache(function(ImageCache $img) use ($image, $ext, $height, $width, $crop, $file_dir) {
+                $img->make($file_dir . $image .'.'. $ext);
 
                 if ($crop) {
                     $img->fit($height, $width);
                 } else {
-                    if ($width and $width < $height) {
-                        $img->widen($width, function ($constraint) {
+                    if ($width and (! $height or $width < $height)) {
+                        $img->widen($width, function (Constraint $constraint) {
                             $constraint->upsize();
                         });
-                    } else {
-                        $img->heighten($height, function ($constraint) {
+                    } elseif ($height) {
+                        $img->heighten($height, function (Constraint $constraint) {
                             $constraint->upsize();
                         });
                     }
                 }
 
             }, $cache_time, true);
+
             return $img->response($ext);
         }
         return abort(404);
